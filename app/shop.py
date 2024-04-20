1f0c89ff0016df8bfa6a4e9db26cda68566e5a2c
kekW <panicua12@gmail.com>
2024-04-20 20:49:27 +0300

    changed Shop to dataclass since Shop doesn't have methods

--- app/shop.py
+++ app/shop.py
@@ -1,8 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Shop:
-    def __init__(self,
-                 name: str,
-                 location: list,
-                 products: dict) -> None:
-        self.name = name
-        self.location = location
-        self.products = products
+    name: str = None
+    location: list[int] = None
+    products: dict[str:float] = None
